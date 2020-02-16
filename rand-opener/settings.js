97a96ce192fbe949d7a8b3e3f77545aaab185b21
www0z0k <www0z.step@gmail.com>
2020-02-17 01:02:50 +0300

    animations

---
+++ rand-opener/settings.js
@@ -0,0 +1,13 @@
+var setPics = [
+	'res/1.jpg',
+	'res/2.jpg',
+	'res/3.jpg',
+	'res/4.jpg',
+	'res/5.jpg',
+	'res/6.jpg',
+	'res/7.jpg',
+	'res/8.jpg'
+];
+
+var question = 'res/q.jpg';
+var error = 'res/qerr.jpg';
